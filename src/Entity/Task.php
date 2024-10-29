da4167dc746f4d6ef448ebd4e425d8fd6a384e74
saldaevvladimir <caldaevvlad3@gmail.com>
2024-10-29 13:10:26 +1000

    added entities: ProjectsGroup, Project, Task

---
+++ src/Entity/Task.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Doctrine\ORM\UuidGenerator;
+use App\Repository\TaskRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Table(name: 'tasks')]
+#[ORM\HasLifecycleCallbacks()]
+class Task {
+
+    #[ORM\Id]
+    #[ORM\Column(type: Uuid::NAME)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?Uuid $id = null;
+
+    public function getId(): ?Uuid {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): self {
+        $this->id = $id;
+    }
+
+    #[ORM\Column(name: 'name', length: 128, nullable: false)]
+    private string $name;
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function setName(string $name): self {
+        $this->name = $name;
+    }
+
+    #[ORM\Column(name: 'description', length: 256)]
+
+    #[ORM\Column(name: 'created_at', type: 'datetime_immutable', options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private \DatetimeImmutable $createdAt;
+
+    public function getCreatedAt(): \DateTimeImmutable {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): self {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    #[ORM\Column(name: 'updated_at', type: 'datetime_immutable', options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private \DateTimeImmutable $updatedAt;
+
+    public function getUpdatedAt(): \DateTimeImmutable {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function updateUpdatedAt(): void {
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+}
+
+?>
