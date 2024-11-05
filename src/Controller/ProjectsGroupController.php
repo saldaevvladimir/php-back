6572880b1228a328741a09b25374f22f0882dd28
saldaevvladimir <caldaevvlad3@gmail.com>
2024-11-06 03:06:02 +1000

    added abstract classes to Forms and to Controllers

--- src/Controller/ProjectsGroupController.php
+++ src/Controller/ProjectsGroupController.php
@@ -1,81 +1,42 @@
 <?php
 
+declare(strict_types= 1);
+
 namespace App\Controller;
 
 use App\Entity\ProjectsGroup;
 use App\Form\ProjectsGroupType;
 use App\Repository\ProjectsGroupRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/projects_group')]
-final class ProjectsGroupController extends AbstractController
-{
-    #[Route(name: 'app_projects_group_index', methods: ['GET'])]
-    public function index(ProjectsGroupRepository $projectsGroupRepository): Response
-    {
-        return $this->render('projects_group/index.html.twig', [
-            'projects_groups' => $projectsGroupRepository->findAll(),
-        ]);
+
+#[Route('/project_groups')]
+final class ProjectsGroupController extends BaseController {
+    #[Route('/', name: 'project_groups', methods: ['GET'], format: 'json')]
+    public function getProjectsGroups(ProjectsGroupRepository $projectsGroupRepository): JsonResponse {
+        return $this->handleGetAll($projectsGroupRepository);
     }
 
-    #[Route('/new', name: 'app_projects_group_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $projectsGroup = new ProjectsGroup();
-        $form = $this->createForm(ProjectsGroupType::class, $projectsGroup);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($projectsGroup);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_projects_group_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('projects_group/new.html.twig', [
-            'projects_group' => $projectsGroup,
-            'form' => $form,
-        ]);
+    #[Route('/add', name: 'add_projects_group', methods: ['POST'], format: 'json')]
+    public function addProjectsGroup(Request $request, EntityManagerInterface $em): JsonResponse {
+        return $this->handleAdd($request, $em, ProjectsGroupType::class, new ProjectsGroup());
     }
 
-    #[Route('/{id}', name: 'app_projects_group_show', methods: ['GET'])]
-    public function show(ProjectsGroup $projectsGroup): Response
-    {
-        return $this->render('projects_group/show.html.twig', [
-            'projects_group' => $projectsGroup,
-        ]);
+    #[Route('/{id}', name: 'get_projects_group', methods: ['GET'], format: 'json')]
+    public function getProjectsGroup(string $id, ProjectsGroupRepository $projectsGroupRepository): JsonResponse {
+        return $this->handleGet($id, $projectsGroupRepository);
     }
 
-    #[Route('/{id}/edit', name: 'app_projects_group_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, ProjectsGroup $projectsGroup, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ProjectsGroupType::class, $projectsGroup);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_projects_group_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('projects_group/edit.html.twig', [
-            'projects_group' => $projectsGroup,
-            'form' => $form,
-        ]);
+    #[Route('/update/{id}', name: 'update_projects_group', methods: ['PATCH'], format: 'json')]
+    public function updateProjectsGroup(string $id, Request $request, EntityManagerInterface $em, ProjectsGroupRepository $projectsGroupRepository): JsonResponse {
+        return $this->handleUpdate($id, $request, $em, ProjectsGroupType::class, $projectsGroupRepository);
     }
 
-    #[Route('/{id}', name: 'app_projects_group_delete', methods: ['POST'])]
-    public function delete(Request $request, ProjectsGroup $projectsGroup, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$projectsGroup->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($projectsGroup);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_projects_group_index', [], Response::HTTP_SEE_OTHER);
+    #[Route('/delete/{id}', name: 'delete_projects_group', methods: ['DELETE'], format: 'json')]
+    public function deleteProjectsGroup(string $id, EntityManagerInterface $em, ProjectsGroupRepository $projectsGroupRepository): JsonResponse {
+        return $this->handleDelete($id, $em, ProjectsGroupType::class, $projectsGroupRepository);
     }
 }
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -1,32 +1,35 @@
 <?php
 
+declare(strict_types= 1);
+
 namespace App\Form;
 
 use App\Entity\Task;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class TaskType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('name')
-            ->add('description')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
-            ])
-        ;
+
+class TaskType extends BaseType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $fieldsWithConstraints = [
+            'name' => [
+                new Assert\NotNull(),
+                new Assert\Length(['min' => 3, 'max' => 128])
+            ],
+            'description' => [
+                new Assert\NotNull(),
+                new Assert\Length(['max'=> 65535]) // text type can hold up to 65535 characters
+            ]
+        ];
+
+        $this->setFieldsWithConstraints($builder, $fieldsWithConstraints);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults([
             'data_class' => Task::class,
+            'csrf_protection' => false,
         ]);
     }
 }
