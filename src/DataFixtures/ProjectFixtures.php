84100070b0a0c449bd34790c1b9f221d2de0fa90
saldaevvladimir <caldaevvlad3@gmail.com>
2025-01-31 23:34:07 +1000

    refactor fixtures to include entity relations

--- src/DataFixtures/ProjectFixtures.php
+++ src/DataFixtures/ProjectFixtures.php
@@ -7,27 +7,35 @@ namespace App\DataFixtures;
 use App\Entity\Project;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Utils\Helpers;
 
 
-class ProjectFixtures extends Fixture {
+class ProjectFixtures extends Fixture implements DependentFixtureInterface {
+    public const PROJECTS = 'projects';
+
     public function load(ObjectManager $manager): void {
-        $date1 = new DateTimeImmutable('2020-01-01');
-        $date2 = new DateTimeImmutable('2022-10-10');
-        $date3 = new DateTimeImmutable('2024-10-10');
-        for ($i = 0; $i < 40; $i++) {
+        $projectsGroups = $this->getReference(ProjectsGroupFixtures::PROJECTS_GROUPS);
+        $projects = [];
+
+        foreach ($projectsGroups as $projectsGroup) {
             $project = new Project();
             $project->setName(Helpers::generateRandomString());
-            $project->setCreatedAt(Helpers::generateDatetimeBetween($date1, $date2));
-            $project->setUpdatedAt(Helpers::generateDatetimeBetween($date2, $date3));
-
+            $project->setProjectsGroup($projectsGroup);
             $manager->persist($project);
+            $projects[] = $project;
         }
 
         $manager->flush();
+        $this->addReference(self::PROJECTS, $projects);
     }
 
+    public function getDependencies(): array {
+        return [
+            ProjectsGroupFixtures::class
+        ];
+    }
 }
 
 ?>
